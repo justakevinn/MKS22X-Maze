48a2920fb83936e3fd9702239f5b3c7273b37509
Kevin <kzong@stuy.edu>
2019-02-28 11:00:20 -0500

    Improving solving the maze

--- Maze.java
+++ Maze.java
@@ -6,10 +6,12 @@ public class Maze{
   private boolean animate;
   private int row;
   private int col;
+  private int[] moves;
 
 
   public Maze(String filename){
     try{
+      moves = new int[]{0, 1, 0, -1, 1, 0, -1, 0};
       row = 0;
       String line = "";
       File text = new File(filename);
@@ -17,7 +19,7 @@ public class Maze{
       while(inf.hasNextLine()){
         line = inf.nextLine();
         row ++;
-        System.out.println(line);//hopefully you can do other things with the line
+      //System.out.println(line);//hopefully you can do other things with the line
       }
       //  System.out.println(col);
       col = line.length();
@@ -64,8 +66,51 @@ public class Maze{
     return s;
   }
 
+  public int solve(){
+    int r = 0;
+    int c = 0;
+    for (int i = 0; i < row; i++){
+      for (int j = 0; j < col; j++){
+        if (maze[i][j] == 'S'){
+          r = i;
+          c = j;
+        }
+      }
+    }
+    System.out.println("r: "+ r + " c: "+ c);
+    solve(r, c);
+    return countPath();
+  }
+
+
+  public boolean solve(int r, int c){
+    if (maze[r][c] == 'E'){
+      return true;
+    }
+    maze[r][c] = '@';
+    for (int i = 0; i < moves.length; i += 2){
+      if (maze[r+moves[i]][c+moves[i+1]] == ' '){
+        maze[r+moves[i]][c+moves[i+1]] = '@';
+        return solve(r+moves[i], c+moves[i+1]);
+      }
+    }
+    for (int i = 0; i < moves.length; i += 2){
+        if (maze[r+moves[i]][c+moves[i+1]] == '@'){
+        maze[r][c] = '.';
+        return solve(r+moves[i], c+moves[i+1]);
+      }
+    }
+    return false;
+  }
+
+  public int countPath(){
+    return 0;
+  }
+
   public static void main(String args[]){
     Maze test = new Maze(args[0]);
+    System.out.println(test.toString());
+    test.solve();
     System.out.println(test.toString());
   }
 }
